43df6c2dd3109dc08d291ed6e1a710ffbf34c4e9
jeldridg <jeldridg@cs.brown.edu>
2013-06-20 17:44:19 -0400

    Updates network delta selection to use new find_best_delta function

--- delta.py
+++ delta.py
@@ -19,44 +19,6 @@ def delta_too_small(component_sizes, max_size ):
     if max_size > max(component_sizes): return True
     else: return False
 
-def find_best_delta_old(permuted_sim, permuted_index, sizes, component_fn, start_quant=0.99):
-    # Construct weighted digraphs for each network for each delta
-    sorted_edges = sorted(sp.ndarray.flatten(permuted_sim))
-    size2delta = dict()
-    for max_size in sizes:
-        # print "\t\tk=", max_size
-        delta = -1
-        index = round(len(sorted_edges)* start_quant)
-        left, right = 0., float(len(sorted_edges))      #TODO: why are these floats rather than ints?
-        visited = []
-        
-        while len(visited) < 100:
-            # print "\t\t\t%s: %s" % (len(visited), index/len(sorted_edges)),
-            # print "(%s)" % format(sorted_edges[int(index)], 'g')
-            
-            # construct graph using new delta
-            delta = sorted_edges[int(index)]
-            DiG = hn.weighted_graph(permuted_sim, permuted_index, delta)
-            if delta in visited:
-                size2delta[max_size] = delta
-                break
-            else:
-                visited.append( delta )
-            
-            # increment / decrement index based on whether components meet size specifications
-            if delta_too_small( get_component_sizes( component_fn( DiG) ), max_size ):
-                right  = index
-                index -= round( (index-left)/2. )
-            else:
-                left   = index
-                index += round( (right-index)/2. )
-
-        if max_size not in size2delta:
-            raise AssertionError("NO DELTA SELECTED FOR k = " + str(max_size))
-            
-    return size2delta
-
-
 def find_best_delta(permuted_sim, permuted_index, sizes, directed, start_quant=0.99):
     component_fn = strong_ccs if directed else nx.connected_components
     # Construct weighted digraphs for each network for each delta
@@ -96,15 +58,15 @@ def find_best_delta(permuted_sim, permuted_index, sizes, directed, start_quant=0
     return size2delta
 
 
-def network_delta_wrapper((network_path, infmat_name, index2gene, tested_genes, h, sizes, component_fn)):
-    permuted_mat = scipy.io.loadmat(network_path)[infmat_name]    
+def network_delta_wrapper((network_path, infmat_name, index2gene, tested_genes, h, sizes, directed)):
+    permuted_mat = scipy.io.loadmat(network_path)[infmat_name]   
     sim_mat, gene_index = create_permuted_sim_mat(permuted_mat, index2gene, tested_genes, h)
-    return find_best_delta_old( sim_mat, gene_index, sizes, component_fn )
+    return find_best_delta(sim_mat, gene_index, sizes, directed )
 
 
 import multiprocessing as mp
 def network_delta_selection(permuted_network_paths, index2gene, infmat_name, tested_genes, h, sizes,
-                            component_fn=strong_ccs, parallel=True):
+                            directed=True, parallel=True):
     print "* Performing network delta selection..."
     if parallel:
         pool = mp.Pool()
@@ -112,7 +74,7 @@ def network_delta_selection(permuted_network_paths, index2gene, infmat_name, tes
     else:
         map_fn = map
         
-    args = [(network_path, infmat_name, index2gene, tested_genes, h, sizes, component_fn) for network_path in permuted_network_paths]
+    args = [(network_path, infmat_name, index2gene, tested_genes, h, sizes, directed) for network_path in permuted_network_paths]
     delta_maps = map_fn(network_delta_wrapper, args)
     
     if parallel:
