aaad5b0bff36a12540a36aef75601144fc88e360
jeldridg <jeldridg@cs.brown.edu>
2013-07-12 17:07:34 -0400

    Data loading for mutation data, MutSig, and MuSiC scores.  No Oncodrive or filtering yet.

---
+++ data.py
@@ -0,0 +1,124 @@
+import hnio
+import hnap
+import sys
+from math import log10
+import scipy
+
+def parse_args(raw_args):
+    description = "Data module"
+    parser = hnap.HotNetArgParser(description=description, fromfile_prefix_chars='@')
+    
+    subparsers = parser.add_subparsers(title='Heat scores')
+
+    #create subparser for options for permuting networks
+    mutation_parser = subparsers.add_parser('mutation', help='Mutation data')
+    mutation_parser.add_argument('--snv_file', required=True, help='SNV file')
+    mutation_parser.add_argument('--cna_file', required=True, help='CNA file')
+    mutation_parser.add_argument('--sample_file', required=True, help='Sample file')
+    mutation_parser.add_argument('--gene_file', required=True, help='Gene file')
+    mutation_parser.add_argument('--min_freq', type=int, default=11, help="Minimum frequency")
+    mutation_parser.add_argument('--output_file', required=True, help='Output file')
+    mutation_parser.set_defaults(heat_fn=mutation)
+    
+    mutsig_parser = subparsers.add_parser('mutsig', help='MutSig scores')
+    mutsig_parser.add_argument('--mutsig_score_file', required=True, help='MutSig score file')
+    mutsig_parser.add_argument('--threshold', type=float, default=1.0, help='Threshold...no idea what this is')
+    mutsig_parser.add_argument('--output_file', required=True, help='Output file')
+    mutsig_parser.set_defaults(heat_fn=mutsig)
+    
+    music_parser = subparsers.add_parser('music', help='MuSiC scores')
+    music_parser.add_argument('--music_score_file', required=True, help='MuSiC score file')
+    music_parser.add_argument('--threshold', type=float, default=1.0, help='Threshold...no idea what this is')
+    music_parser.add_argument('--max_heat', type=float, default=15, help='Max heat')
+    music_parser.add_argument('--output_file', required=True, help='Output file')
+    music_parser.set_defaults(heat_fn=music)
+    
+    return parser.parse_args(raw_args)
+
+def num_snvs(mutation_list):
+    return len([ p for p, muts in mutation_list.items() if "snv" in muts ])
+
+def num_cnas(mutation_list):
+    return len([ p for p, muts in mutation_list.items() if "amp" in muts or "del" in muts ])
+
+def mut_heat((genes, samples, gene2mutations), min_freq):
+    print "* Creating mutation fraction heat map..."
+    n = float(len(samples))
+    heat = dict([(g, len( heat ) / n) for g, heat in gene2mutations.items()
+                 if num_snvs(heat) > min_freq or num_cnas(heat) > 0])
+    genes = sorted( heat.keys() )
+    samples = set([ s for g in genes for s in gene2mutations[g] ])
+    print "\t- Including", len(genes), "genes at min frequency", min_freq
+    print "\t- Samples with no mutations (not taken into account for computing",
+    print "heat):", int(n) - len(samples)
+    return genes, heat
+
+def mutsig_heat(gene2mutsig, threshold=1.0):
+    print "* Creating MutSig heat map..."
+    gene2heat = dict([(gene, -log10(score["qval"]))
+                      for gene, score in gene2mutsig.items()
+                      if score["qval"] < threshold])
+    print "\t- Including", len(gene2heat), "genes at threshold", threshold
+    return gene2heat.keys(), gene2heat
+
+def music_heat(gene2music, threshold=1.0, max_heat=15):
+    print "* Creating MuSiC heat map..."
+    print "\t- Mapping q-values of 0 to", max_heat
+    def music_heat(qvals):
+        heat = scipy.median([ qvals["FDR_CT"], qvals["FDR_LRT"], qvals["FDR_FCPT"] ])
+        return -log10(heat) if heat != 0 else max_heat
+    gene2heat = dict([(gene, music_heat(scores)) for gene, scores in gene2music.items()
+                      if scipy.median(scores.values()) < threshold])
+    print "\t- Including", len(gene2heat), "genes at threshold", threshold
+    return gene2heat.keys(), gene2heat
+ 
+def mutation(args):
+    genes_samples_gene2heat, samples_w_tys = hnio.load_mutation_data(args.snv_file, args.cna_file, args.sample_file, args.gene_file)
+    return mut_heat(genes_samples_gene2heat, args.min_freq)
+
+def mutsig(args):
+    gene2mutsig = hnio.load_mutsig_scores(args.mutsig_score_file)
+    return mutsig_heat(gene2mutsig, args.threshold)
+
+def music(args):
+    gene2music = hnio.load_music_scores(args.music_score_file)
+    return music_heat(gene2music, args.threshold, args.max_heat)
+
+def run(args):
+    genes, heat = args.heat_fn(args)
+    hnio.save_heat(heat, args.output_file);
+
+if __name__ == "__main__": 
+    run(parse_args(sys.argv[1:]))
+
+# NULL = 100
+# from math import log10
+# def fm_heat(gene2heat,fm_threshold, cis_threshold=0.01, CIS=False):
+#     print "* Creating oncodrive heat map..."
+#     if CIS: print "\tIncluding CIS scores at threshold", cis_threshold, "..."
+#     heat = dict()
+#     src_fm, src_cis_amp, src_cis_del = 0, 0, 0
+#     for g, scores in gene2heat.items():
+#         if CIS:
+#             del_score = scores["del"] if scores["del"] < cis_threshold else NULL
+#             amp_score = scores["amp"] if scores["amp"] < cis_threshold else NULL
+#             fm_score  = scores["fm"] if scores["fm"] < fm_threshold else NULL
+#             if fm_score == NULL and amp_score == NULL and del_score == NULL: continue
+#             min_val = min(del_score, amp_score, fm_score)
+#             heat[g] = -log10( min_val )
+#             if min_val == scores["fm"]: src_fm += 1
+#             elif min_val == scores["amp"]: src_cis_amp += 1
+#             elif min_val == scores["del"]: src_cis_del += 1
+#         else:
+#             if scores["fm"] >= fm_threshold: continue
+#             heat[g] = -log10(scores["fm"])
+#             src_fm += 1
+#     print "\t- Genes using FM score:", src_fm
+#     print "\t- Genes using CIS AMP score:", src_cis_amp
+#     print "\t- Genes using CIS DEL score:", src_cis_del
+# 
+#     return sorted(heat.keys()), heat
+#  
+# def expr_filter_heat( gene2heat ):
+#     gene2heat = dict([(g, h) for g, h in gene2heat.items() if g in EXPRESSED_GENES])
+#     return gene2heat.keys(), gene2heat
--- hnio.py
+++ hnio.py
@@ -12,6 +12,86 @@ def load_heat(heat_file):
 def load_gene_list(gene_list_file):
     return set([l.strip() for l in open(gene_list_file)])
 
+def load_mutation_data(snv_file, cna_file, sample_file, gene_file):
+    print "* Loading mutation data..."
+    # Load whitelist
+    gene_wlst = set([ l.rstrip() for l in open(gene_file) ])
+    sample_wlst = set([ l.rstrip().split()[0] for l in open(sample_file) ])
+    samples_w_tys = [ tuple( l.rstrip().split()) for l in open(sample_file) ]
+
+    # Load SNVs
+    print "\tLoading SNVs..."
+    arrs = [l.rstrip().split("\t") for l in open(snv_file)
+            if not l.startswith("#")]
+    sample2snvs = dict([(arr[0], set(arr[1:]) & gene_wlst) for arr in arrs
+                        if arr[0] in sample_wlst])
+    samples = set( sample2snvs.keys() )
+    genes = set([ g for _, snvs in sample2snvs.items() for g in snvs])
+    genes = genes & gene_wlst
+    gene2heat = dict([(g, dict()) for g in genes])
+    for sample, snvs in sample2snvs.items():
+        for g in snvs:
+            gene2heat[g][sample] = [ "snv" ]
+
+    # Load CNAs
+    print "\tLoading CNAs..."
+    arrs = [l.rstrip().split("\t") for l in open(cna_file)
+            if not l.startswith("#")]
+    arrs = [arr for arr in arrs if arr[0] in sample_wlst]
+    
+    new_genes = set([ g.split("(")[0] for a in arrs for g in a[1:]])
+    new_genes = new_genes & gene_wlst
+    for g in set( new_genes ) - set( gene2heat.keys() ): gene2heat[g] = dict()
+    genes = set(gene2heat.keys())
+    for arr in arrs:
+        sample, mutations = arr[0], set([g for g in arr[1:]
+                                          if g.split("(")[0] in gene2heat.keys() ])
+        samples.add( sample )
+        for g in mutations:
+            
+            if g.endswith("(A)"): mut_type = "amp"
+            elif g.endswith("(D)"): mut_type = "del"
+            gene_name = g.split("(")[0]
+                
+            if sample in gene2heat[gene_name].keys():
+                gene2heat[gene_name][sample].append( mut_type )
+            else:
+                gene2heat[gene_name][sample] = [ mut_type ]
+    genes = set(gene2heat.keys())
+
+    print "\tLoaded mutation data for", len(genes),
+    print "genes in %s samples" % len(samples)
+
+    sample_w_tys = [(s, ty) for s, ty in samples_w_tys if s in samples]     #MAX: what's the deal with this line?
+
+    return (genes, samples, gene2heat), samples_w_tys
+
+def load_mutsig_scores( scores_file ):
+    arrs = [l.rstrip().split("\t") for l in open(scores_file)
+            if not l.startswith("#")]
+    print "* Loading MutSig scores in", len(arrs), "genes..."
+    return dict([(arr[0], {"pval": float(arr[-2]), "qval": float(arr[-1])})
+                 for arr in arrs])
+
+
+FDR_CT, FDR_LRT, FDR_FCPT = 12, 11, 10
+music_score2name = {FDR_CT: "FDR_CT", FDR_LRT: "FDR_LRT", FDR_FCPT: "FDR_FCPT"}
+def load_music_scores(scores_file):
+    print "* Loading MuSiC scores using the median of the 3 q-values..."
+    # Load file and tab-split lines 
+    arrs = [l.rstrip().split("\t") for l in open(scores_file)
+            if not l.startswith("#")]
+
+    # Indices for the columns we may be interested in
+    gene2music = dict([(arr[0], {"FDR_CT": float(arr[FDR_CT]),
+                                 "FDR_FCPT": float(arr[FDR_FCPT]),
+                                 "FDR_LRT":float(arr[FDR_LRT])})
+                       for arr in arrs])
+
+    # Output parsing info
+    print "\t- Loaded %s genes." % len(gene2music)
+    return gene2music
+
 ################################################################################
 # Data saving functions
 
