cddc312072f2f48abd0a5c4082053a06306824c4
jeldridg <jeldridg@cs.brown.edu>
2013-07-21 19:07:54 -0400

    Updates arg parser to use shlex.split (preserves quoted strings as single arguments)

--- hnap.py
+++ hnap.py
@@ -1,7 +1,8 @@
 import argparse
+import shlex
 
 class HotNetArgParser(argparse.ArgumentParser):
     def convert_arg_line_to_args(self, arg_line):
         if not arg_line.startswith('#'):
-            for arg in arg_line.split():
+            for arg in shlex.split(arg_line):
                 yield arg
