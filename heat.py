471c7d42553d3cc3a9d72c5585597e5f7ac69d7f
jeldridg <jeldridg@cs.brown.edu>
2013-08-03 19:09:54 -0400

    Adds docstring comments for mutation-related methods in heat.py and removes unused mut_heat_old

--- heat.py
+++ heat.py
@@ -4,12 +4,33 @@ import scipy
 from constants import *
 
 def num_snvs(mutations):
+    """Return the number of SNVs in the given iterable of Mutations (those with mut_type == SNV).
+    
+    Arguments:
+    mutations -- iterable of Mutation tuples 
+    
+    """
     return len([mut for mut in mutations if mut.mut_type == SNV])
 
 def num_cnas(mutations):
+    """Return the number of CNAs in the given iterable of Mutations (those with mut_type == AMP or DEL).
+    
+    Arguments:
+    mutations -- iterable of Mutation tuples 
+    
+    """
     return len([mut for mut in mutations if mut.mut_type == AMP or mut.mut_type == DEL])
 
 def filter_cnas(cnas, filter_thresh):
+    """Return a list of Mutation tuples containing only CNAs in genes in which the proportion of
+    CNAs in the gene across samples is at least filter_thresh. CNAs in genes whose CNAs pass the
+    threshold that are of the opposite type of the dominant type in the gene are also excluded.
+    
+    Arguments:
+    cnas -- a list of Mutation tuples representing CNAs. All are assumed to be of mut_type AMP or DEL
+    filter_thresh -- the proportion of CNAs in a gene that must be of the same type
+    
+    """
     if filter_thresh <= .5:
         raise ValueError("filter_thresh must be greater than .5")
     
@@ -37,6 +58,17 @@ def remove_opposite_cnas(cnas, gene_cnas, mut_type):
             cnas.remove(cna)
 
 def mut_heat(num_samples, snvs, cnas, min_freq):
+    """Return a dict mapping gene name to heat score based on the given mutation data.
+    
+    Arguments:
+    num_samples -- the number of samples tested for mutations
+    snvs -- iterable of Mutation tuples representing SNVs (mut_type == SNV)
+    cnas -- iterable of Mutation tuples representing CNAs (mut_type == AMP or DEL)
+    min_freq -- the minimum number of samples in which a gene must have an SNV to be considered
+                mutated in the heat score calculation.
+    
+    """
+    
     genes2mutations = defaultdict(set)
     for snv in snvs:
         genes2mutations[snv.gene].add(snv)
@@ -47,22 +79,6 @@ def mut_heat(num_samples, snvs, cnas, min_freq):
           (len(genes2mutations), num_samples, min_freq))
     
     return dict([(g, len(heat) / float(num_samples)) for g, heat in genes2mutations.items()
-                 if num_snvs(heat) >= min_freq or num_cnas(heat) > 0])
-
-def mut_heat_old(samples2snvs, samples2cnas, min_freq):
-    n = float(len(set(samples2snvs.keys()) | set(samples2cnas.keys())))
-    
-    genes2mutations = defaultdict(set)
-    snvs = reduce(lambda acc, upd: acc.union(upd), samples2snvs.values(), set())
-    cnas = reduce(lambda acc, upd: acc.union(upd), samples2cnas.values(), set())
-    for snv in snvs:
-        genes2mutations[snv.gene].add(snv)
-    for cna in cnas:
-        genes2mutations[cna.gene].add(cna)
-    
-    print "\t- Including %s genes in %s samples at min frequency %s" % (len(genes2mutations), int(n), min_freq)
-    
-    return dict([(g, len( heat ) / n) for g, heat in genes2mutations.items()
                  if num_snvs(heat) >= min_freq or num_cnas(heat) > 0])
 
 NULL = 100
