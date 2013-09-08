011bde53ba5fa3a0f5a1cb651a5e20ada92fbdd0
jeldridg <jeldridg@cs.brown.edu>
2013-09-08 18:04:34 -0400

    Adds support for running multiple deltas to runHotnet2.py

--- runHotnet2.py
+++ runHotnet2.py
@@ -28,7 +28,7 @@ def parse_args(raw_args):
                               column of each line.')
     parser.add_argument('-hf', '--heat_file', required=True,
                         help='JSON heat score file generated via generateHeat.py')
-    parser.add_argument('-d', '--delta', type=float, required=True,
+    parser.add_argument('-d', '--deltas', nargs='+', type=float, required=True,
                         help='Weight threshold for edge removal')
     parser.add_argument('-ccs', '--min_cc_size', type=int, default=3,
                         help='Minimum size connected components that should be returned.')
@@ -99,45 +99,45 @@ def run(args):
     h = hn.heat_vec(heat, gene_index)
     sim = hn.similarity_matrix(M, h, not args.classic)
     
-    delta = args.delta
-    delta_out_dir = args.output_directory + "/delta_" + str(delta)
-    if not os.path.isdir(delta_out_dir):
-        os.mkdir(delta_out_dir)
-    
-    G = hn.weighted_graph(sim, gene_index, args.delta, not args.classic)
-    ccs = hn.connected_components(G, args.min_cc_size)
-    
-    # calculate significance
-    if args.permutation_type != "none":
-        if args.permutation_type == "heat":
-            sizes2stats = heat_permutation_significance(args, heat, infmat, infmat_index, G)
-        elif args.permutation_type == "mutations":
-            if heat_params["heat_fn"] != "load_mutation_heat":
-                raise RuntimeError("Heat scores must be based on mutation data to perform\
-                                    significance testing based on mutation data permutation.")
-            sizes2stats = mutation_permutation_significance(args, infmat, infmat_index, G, heat_params)
-        else:
-            raise ValueError("Unrecognized permutation type %s" % (args.permutation_type))
-    
-    #sort ccs list such that genes within components are sorted alphanumerically, and components
-    #are sorted first by length, then alphanumerically by name of the first gene in the component 
-    ccs = [sorted(cc) for cc in ccs]
-    ccs.sort(key=lambda comp: comp[0])
-    ccs.sort(key=len, reverse=True)
-    
-    #write output
-    hnio.write_components_as_tsv(os.path.abspath(delta_out_dir) + "/" + COMPONENTS_TSV, ccs)
-    
-    output_dict = {"parameters": vars(args), "heat_parameters": heat_params,
-                   "sizes": hn.component_sizes(ccs), "components": ccs}
-    if args.permutation_type != "none":
-        output_dict["statistics"] = sizes2stats
-        hnio.write_significance_as_tsv(os.path.abspath(delta_out_dir) + "/" + SIGNIFICANCE_TSV,
-                                       sizes2stats)
-    
-    json_out = open(os.path.abspath(delta_out_dir) + "/" + JSON_OUTPUT, 'w')
-    json.dump(output_dict, json_out, indent=4)
-    json_out.close()
+    for delta in args.deltas:
+        delta_out_dir = args.output_directory + "/delta_" + str(delta)
+        if not os.path.isdir(delta_out_dir):
+            os.mkdir(delta_out_dir)
+        
+        G = hn.weighted_graph(sim, gene_index, delta, not args.classic)
+        ccs = hn.connected_components(G, args.min_cc_size)
+        
+        # calculate significance
+        if args.permutation_type != "none":
+            if args.permutation_type == "heat":
+                sizes2stats = heat_permutation_significance(args, heat, infmat, infmat_index, G)
+            elif args.permutation_type == "mutations":
+                if heat_params["heat_fn"] != "load_mutation_heat":
+                    raise RuntimeError("Heat scores must be based on mutation data to perform\
+                                        significance testing based on mutation data permutation.")
+                sizes2stats = mutation_permutation_significance(args, infmat, infmat_index, G, heat_params)
+            else:
+                raise ValueError("Unrecognized permutation type %s" % (args.permutation_type))
+        
+        #sort ccs list such that genes within components are sorted alphanumerically, and components
+        #are sorted first by length, then alphanumerically by name of the first gene in the component 
+        ccs = [sorted(cc) for cc in ccs]
+        ccs.sort(key=lambda comp: comp[0])
+        ccs.sort(key=len, reverse=True)
+        
+        #write output
+        hnio.write_components_as_tsv(os.path.abspath(delta_out_dir) + "/" + COMPONENTS_TSV, ccs)
+        
+        output_dict = {"parameters": vars(args), "heat_parameters": heat_params,
+                       "sizes": hn.component_sizes(ccs), "components": ccs}
+        if args.permutation_type != "none":
+            output_dict["statistics"] = sizes2stats
+            hnio.write_significance_as_tsv(os.path.abspath(delta_out_dir) + "/" + SIGNIFICANCE_TSV,
+                                           sizes2stats)
+        
+        json_out = open(os.path.abspath(delta_out_dir) + "/" + JSON_OUTPUT, 'w')
+        json.dump(output_dict, json_out, indent=4)
+        json_out.close()
 
 def heat_permutation_significance(args, heat, infmat, infmat_index, G):
     print "* Performing permuted heat statistical significance..."
