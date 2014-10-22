35544cf551adabd10a977473dc1d9bee8cd42bea
Mohammed El-Kebir <m.elkebir@gmail.com>
2014-10-22 15:09:53 -0400

    compute_sim_classic function

--- c_routines.c
+++ c_routines.c
@@ -1,3 +1,5 @@
+#include <math.h>
+
 #define index_in(a, i, j) a[i*p + j]
 #define index_out(a, i, j) a[i*q + j]
 
@@ -9,6 +11,20 @@ void compute_sim(double *infmat, double *h, long *indices, long p, long q, doubl
     for (j = 0; j < q; ++j)
     {
       index_out(M, i, j) = index_in(infmat, indices[i], indices[j]) * h[j];
+    }
+  }
+}
+
+void compute_sim_classic(double *infmat, double *h, long *indices, long p, long q, double *M)
+{
+  int i, j;
+  for (i = 0; i < q; ++i)
+  {
+    for (j = 0; j < q; ++j)
+    {
+      index_out(M, i, j) = index_out(M, j, i) = fmin(index_in(infmat, indices[i], indices[j]),
+                                                     index_in(infmat, indices[j], indices[i])) 
+                                                * fmax(h[i], h[j]);
     }
   }
 }
