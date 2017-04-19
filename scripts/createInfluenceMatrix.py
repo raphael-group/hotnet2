ccb3559254f3452917b1f1b4b4e10f13956cd958
Matthew Reyna <matthew_reyna@brown.edu>
2017-04-19 09:35:34 -0400

    Actually resolved conflict

--- scripts/createInfluenceMatrix.py
+++ scripts/createInfluenceMatrix.py
@@ -4,7 +4,7 @@
 import sys, os, numpy as np, networkx as nx, scipy as sp, scipy.io
 sys.path.append(os.path.normpath(os.path.dirname(os.path.realpath(__file__)) + '/../'))
 <<<<<<< HEAD
-from hotnet2 import * 
+from hotnet2 import *
 =======
 from hotnet2 import *
 >>>>>>> 944ea8c9435b224cf4d1a2b12225330b5f167b2c
@@ -26,26 +26,17 @@ def get_parser():
     parser.add_argument('-v', '--verbose', required=False, default=0, type=int, choices=range(5),
                     help="Control verbosity of output.")
 
-    # Subparsers for the different 
+    # Subparsers for the different
     subparser = parser.add_subparsers(dest='diffusion_type', help='Diffusion method')
 
     hn2_parser = subparser.add_parser(HOTNET2)
     hn2_parser.add_argument('-b', '--beta', required=True, type=float, help="Restart probability beta.")
-    
+
     hn_parser = subparser.add_parser(HOTNET)
     hn_parser.add_argument('-t', '--time', required=True, type=float, help='Diffusion time.')
 
     return parser
 
-<<<<<<< HEAD
-=======
-# Remove self-loops, multi-edges, and restrict to the largest component
-def largest_component(G):
-    selfLoops = [(u, v) for u, v in G.edges() if u == v]
-    G.remove_edges_from( selfLoops )
-    return G.subgraph( sorted(nx.connected_components( G ), key=lambda cc: len(cc), reverse=True)[0] )
-
->>>>>>> 944ea8c9435b224cf4d1a2b12225330b5f167b2c
 def run(args):
     params = dict(network_name=args.network_name)
     if args.diffusion_type == HOTNET2:
@@ -56,6 +47,6 @@ def run(args):
         raise NotImplementedError('Diffusion of type "%s" not implemented' % args.diffusion_type)
     save_diffusion_to_file(args.diffusion_type, diffusion_param, args.gene_index_file, args.edgelist_file,
                            args.output_file, params=params, verbose=args.verbose)
-        
+
 if __name__ == "__main__":
     run(get_parser().parse_args(sys.argv[1:]))
